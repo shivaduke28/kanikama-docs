e98ad9d9f86b7ddef2598f66ea7a784779a143e7
shivaduke28 <shivaduke28@gmail.com>
2025-02-03 04:32:59 +0900

    update icons

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -68,10 +68,10 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Kanikama Docs',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'Kanikama Icon',
+        src: 'img/favicon-512.png',
       },
       items: [
         {
